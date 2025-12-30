ebd84e1ca2969c43b5277e20ecf6dc983420224a
juanjo1213 <juanjorafaangel@gmail.com>
2025-12-30 23:31:27 +0100

    Ignore Python cache files and add anylisis file

---
+++ analysis/validation.py
@@ -0,0 +1,43 @@
+import numpy as np
+from src.montecarlo import montecarlo_attenuation
+
+def validate_attenuation(
+        N,
+        mu,
+        x_max,
+        bins=50,
+        seed=123
+
+):
+    
+    """
+    Validates Monte carlos attenuation against analytical solution.
+    """
+
+    # --- Monte Carlo simulation----
+    results=montecarlo_attenuation(
+        N=N,
+        mu=mu,
+        x_max=x_max,
+        bins=bins,
+        seed=seed
+    )
+
+    T_mc=results["transmitted_fraction"]
+    sigma_mc=results["statistical_error"]
+
+    #---Analytical solution---
+    T_analytical=np.exp(-mu*x_max)
+
+    #---Errors---
+    absolute_error=abs(T_mc-T_analytical)
+    relative_error=absolute_error/T_analytical
+
+
+    return{
+        "T_mc":T_mc,
+        "T_analytical": T_analytical,
+        "Absolute_error":absolute_error,
+        "Relative_error":relative_error,
+        "Statistical_error":sigma_mc
+    }
